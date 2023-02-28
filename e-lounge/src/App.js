d754aabd2dd1bfc3970423b123d41616ede924f0
markymark13344 <116988269+markymark13344@users.noreply.github.com>
2023-02-27 21:05:44 -0600

    Navbar and CSS
    Added in navbar and tested a css animation

--- e-lounge/src/App.js
+++ e-lounge/src/App.js
@@ -1,10 +1,19 @@
 import React from 'react';
+import "bootstrap/dist/css/bootstrap.min.css";
+import "./custom.css"
 
 
 function App() {
   return (
     <div className="App">
-      <h1>Testing</h1>
+      <div>
+        <nav className='navbar navbar-expand-lg navbar-light bg-light'>
+          <div className='container-fluid'>
+            <a className='navbar-brand logo' href='/#'><img src='logo192.png' alt='Testing logo'/></a>
+          </div>
+        </nav>
+      </div>
+      
     </div>
   );
 }
