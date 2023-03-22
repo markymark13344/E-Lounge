8c7f3af3542fccc7d91b18a63c86b23a71050275
markymark13344 <116988269+markymark13344@users.noreply.github.com>
2023-03-22 15:43:32 -0500

    Routing
    Added routing and styled components

--- e-lounge/src/App.jsx
+++ e-lounge/src/App.jsx
@@ -1,33 +1,31 @@
 import Landing from "./pages/Landing";
 import logo from './assets/images/E-lounge/android-chrome-192x192.png';
+import {BrowserRouter, Routes, Route, Link} from 'react-router-dom';
 
 
 function App() {
   return (
     <div className="App">
       <div>
-
-      
-
-      <nav className="navbar navbar-dark d-flex flex-row">
-        <div>
-        <a className="navbar-brand" href="/#">
-          <img className="p-2" src={logo} alt="logo"/>
-        </a>
-        <span className="brandName">E-LOUNGE</span>
-        </div>
-       
-        
-       </nav>
-
-
-
+        <nav className="navbar navbar-dark d-flex flex-row border border-dark">
+          <div>
+          <a className="navbar-brand" href="/#">
+            <img className="brandLogo" src={logo} alt="logo"/>
+          </a>
+          <span className="brandName">E-LOUNGE</span>
+          </div>
+        </nav>
       </div>
-      
 
-      
-      <Landing />
+      <BrowserRouter >
+        <Routes>
+          <Route path='/' element={<Landing/>} />
+        </Routes>
+      </BrowserRouter>
     </div>
+
+    
+    
   );
 }
 
